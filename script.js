7a2405d79f0d14983a803369ba3bbe29fa83eeb0
Owen F. O'Connor <148789982+ThatObiGuy@users.noreply.github.com>
2025-02-14 10:50:53 +0000

    Update script.js 14/02/2025
    > Improved pause button
    > Start now also resumes pause
    > Timer ended detection fixed - previously not working
    > developed 1-min remaining event
    > little tweaks

--- script.js
+++ script.js
@@ -10,11 +10,29 @@ var sec = 0;
 var min = 25;
 var toggle = 0; // used for toggling pause - could have alternatively used boolean
 var session = null; // allows us to ensure only 1 timer session is running
+var started = false; // Allows us to make pause button useless until timer started with start button
+var audio = new Audio('ding.mp3'); // Timer is for studying, so audio que for finishing may be helpful
+
+function changeBorderBlue() { // Extra functions added to facilitate requirement, word in conjuction with 1 min remaining event
+    var timeComponents = document.getElementsByClassName('time'); // Saves the min/sec/milisec to an array
+    for(i = 0; i < timeComponents.length; i++) { // loops through array
+        timeComponents[i].style.backgroundColor = '#87CEFA'; // sets each element to have the same background colour
+    }  
+} // turns border text of timer to blue, as we're required to have a border around our timer at this time
+
+function changeBorderRed() { // Extra functions added to facilitate requirement, word in conjuction with 1 min remaining event
+    var timeComponents = document.getElementsByClassName('time');
+    for(i = 0; i < timeComponents.length; i++) {
+        timeComponents[i].style.backgroundColor = '#FF5146';
+    }  
+} // necessary for reset when we stop timer, changing border colour back to something more suitable for the theme
 
 function setRed(){ // Here we change CSS elements using javascript, nice. This occurs when there's 5 minutes left
     document.getElementById('minutes').style.color = "red";
     document.getElementById('seconds').style.color = "red";
     document.getElementById('miliseconds').style.color = "red";
+    document.getElementById('leftColon').style.color = "red";
+    document.getElementById('rightColon').style.color = "red";
     document.getElementById('appleBase').style.fill = "#1E90FF";
     document.getElementById('appleDepth').style.fill = "#4682B4";
     document.getElementById('appleLeftShine').style.fill = "#87CEFA";
@@ -23,6 +41,8 @@ function setRed(){ // Here we change CSS elements using javascript, nice. This o
     document.getElementById('appleRightShade').style.fill = "#1C86EE";
     document.getElementById('appleDephtShade').style.fill = "#5F9EA0";
     document.getElementById('appleDephtDeepShade').style.fill = "#27408B";
+    document.getElementById('appleDephtDeepShade').style.fill = "#27408B";
+    changeBorderBlue()
 
 }// Not required in spec but we turn the apple blue when the text turns red to make it pop
 
@@ -30,6 +50,8 @@ function setWhite(){ // This is necessary for the reset when we hit the stop but
     document.getElementById('minutes').style.color = "white";
     document.getElementById('seconds').style.color = "white";
     document.getElementById('miliseconds').style.color = "white";
+    document.getElementById('leftColon').style.color = "white";
+    document.getElementById('rightColon').style.color = "white";
     document.getElementById('appleBase').style.fill = "#E12C00";
     document.getElementById('appleDepth').style.fill = "#F6520A";
     document.getElementById('appleLeftShine').style.fill = "#FF5146";
@@ -38,12 +60,13 @@ function setWhite(){ // This is necessary for the reset when we hit the stop but
     document.getElementById('appleRightShade').style.fill = "#CC1300";
     document.getElementById('appleDephtShade').style.fill = "#A94804";
     document.getElementById('appleDephtDeepShade').style.fill = "#7A5000";
+    changeBorderRed()
 
 }
 
 function decreaseMilisec(){
     if (milisec == 0) {
-        milisec = 10; // our code only runs 10 times per milisecond - we can only see so many numbers per ms + refreshrates anyways
+        milisec = 10; // our code only runs 10 times per milisecond - we can only see so many numbers per ms + refresh rates anyways
         decreaseSec();
     }
     milisec--;
@@ -52,9 +75,19 @@ function decreaseMilisec(){
 }
 
 function decreaseSec(){
+    if(min == 0){ // these nested if statements check for the timer depleated condition
+        if(sec == 0){
+            audio.play();
+            stopTimer(); // Timer resets on the page
+            alert("Take a break."); // User gets a pop-up telling them to take a break
+
+        }
+
+    }
     if (sec == 0) {
         sec = 60;
         decreaseMin();
+
     }
     sec--;
     document.getElementById("seconds").innerText = sec;
@@ -63,12 +96,8 @@ function decreaseSec(){
 
 function decreaseMin(){
     min--;
-    if (min == 1) { // Visual change lets user know timer is almost up
+    if (min == 0) { // Visual change lets user know timer is almost up
         setRed()
-    }
-    if (min == 0) {
-        alert("Take a break.");
-        stopTimer();
     }
     document.getElementById("minutes").innerText = min;
 
@@ -77,26 +106,34 @@ function decreaseMin(){
 function startTimer(){ // For the start button
     if (session == null) { // if session == null checks if there's already a timer active, we only want one running at a time
         session = setInterval(decreaseMilisec, 100);  // if we set interval to 100 we only need to display 10 integers for miliseconds
+        started = true;
+        if(toggle%2 != 0) { // start also resumes
+            toggle++; // increments to keep in fashion with pause
+            document.getElementById("pause").innerText = "pause"; // reverts the button back to pause
+        }
     }
 
 }
 
 function pauseTimer(){ // For the pause button
-    toggle++; // Works on even and odd measurements for pause and resume
-    if (toggle%2 == 0) { // On even values we'll resume
-        startTimer(); // may aswell call the function as it also checks for other sessions, efficiency
-        document.getElementById("pause").innerText = "pause"; // Now we've resumed we prompt user to pause
-    } else { // On odd values we'll pause, makes sense as press numero uno will then be a pause
-        clearInterval(session); // clearing session means we're no longer calling decrease milisecond every 100 miliseconds
-        session = null; // If the session still exsists, we won't be able to resume
-        document.getElementById("pause").innerText = "resume"; // Now we're paused so we can prompt user to resume
+    if (started) { // pause button will not work until timer is started
+        toggle++; // Works on even and odd measurements for pause and resume
+        if (toggle%2 == 0) { // On even values we'll resume
+            startTimer(); // may aswell call the function as it also checks for other sessions, efficiency
+            document.getElementById("pause").innerText = "pause"; // Now we've resumed we prompt user to pause
+        } else { // On odd values we'll pause, makes sense as press numero uno will then be a pause
+            clearInterval(session); // clearing session means we're no longer calling decrease milisecond every 100 miliseconds
+            session = null; // If the session still exsists, we won't be able to resume
+            document.getElementById("pause").innerText = "resume"; // Now we're paused so we can prompt user to resume
+        }
     }
-
+    
 }
 
 function stopTimer(){ // For the stop button - the great reset - we reset most variables to the initialised values
     clearInterval(session); // Stop the countdown
     session = null; // Reset the session
+    started = false; // The timer has not start, we must reflect that so pause button cannot be abused
     milisec = 0;
     sec = 0;
     min = 25; // Reset time
